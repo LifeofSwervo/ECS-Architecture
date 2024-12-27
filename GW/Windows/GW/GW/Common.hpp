79fcb228051e7525ad5f4a1c10b1743c58a3aa45
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-12-26 20:08:04 -0600

    Successful import

---
+++ GW/Windows/GW/GW/Common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+// Common.hpp
+
+#include <raylib.h>
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <fstream> // File Stream
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <stdio.h>
+
+//#include "Vec2.hpp"
+
+template <class T> using ptr = std::shared_ptr<T>;
