fda36d5ea02ff5e5f29bfde1fbf3bff20bf697ee
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2025-02-11 10:12:16 -0600

    Library update (SDL Base screen)

---
+++ Library/SDL/Base Screen/Vec2.cpp
@@ -0,0 +1,65 @@
+#include "Vec2.hpp"
+#include <cmath>
+
+Vec2::Vec2() : x(0), y(0) {}
+
+Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
+
+Vec2 Vec2::operator + (const Vec2& rhs) const
+{
+    return Vec2(x + rhs.x, y + rhs.y);
+}
+
+Vec2 Vec2::operator - (const Vec2& rhs) const
+{
+    return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator * (const float val) const
+{
+    return Vec2(x * val, y * val);
+}
+
+Vec2 Vec2::operator / (const float val) const
+{
+    return Vec2(x / val, y / val);
+}
+
+bool Vec2::operator == (const Vec2& rhs) const
+{
+    return x == rhs.x && y == rhs.y;
+}
+
+bool Vec2::operator != (const Vec2& rhs) const
+{
+    return !(*this == rhs);
+}
+
+void Vec2::operator += (const Vec2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+}
+
+void Vec2::operator -= (const Vec2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+void Vec2::operator *= (const float val)
+{
+    x *= val;
+    y *= val;
+}
+
+void Vec2::operator /= (const float val)
+{
+    x /= val;
+    y /= val;
+}
+
+float Vec2::dist(const Vec2& rhs) const
+{
+    return std::sqrt((x - rhs.x) * (x - rhs.x) + (y - rhs.y) * (y - rhs.y));
+}
